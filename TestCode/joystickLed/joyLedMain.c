c9146b54261db64fd8040dc61879bbbc08ab3f31
marcoy <marcoy@1dc674fe-e43a-11dd-95e1-755cbddbdd66>
2009-01-20 17:29:00 +0000

    Added some test code to demonstrate how to toggle the LEDs using the joystick.
    This code is using interrupts instead of the polling.

---
+++ TestCode/joystickLed/joyLedMain.c
@@ -0,0 +1,70 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  jobLedMain.c
+ *
+ *    Description:  Using joystick to toggle the LEDs
+ *
+ *         Author:  Marco Yuen <marcoy@gmail.com>
+ *
+ * =====================================================================================
+ */
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define NO_PRESCALE() CLKPR=(1<<CLKPCE); \
+                      CLKPR=0x00
+
+inline void __attribute__((always_inline))
+setupInterrupts(void)
+{
+    // INT4 - Right
+    // First set to detect any logical change on INT4
+    EICRB &= ~(_BV(ISC41));
+    EICRB |= _BV(ISC40);
+    // Enable INT4
+    EIMSK |= _BV(INT4);
+
+    // INT5 - Down
+    // First set to detect any logical change on INT5
+    EICRB &= ~(_BV(ISC51));
+    EICRB |= _BV(ISC50);
+    // Enable INT5
+    EIMSK |= _BV(INT5);
+}
+
+int
+main(int argc, char *argv[])
+{
+    cli();         // Disable interrupts
+
+    NO_PRESCALE(); // Turn off system clock prescaler so it can run at 8Mhz
+    setupInterrupts();
+
+    sei();         // Enable interrupts
+
+    // Enable the pins for input and pull them up
+    DDRB  = 0x00;
+    PORTB = 0xff;
+    DDRE  = 0x00;
+    PORTE = 0xff;
+
+    DDRD  = 0xff;  // Set LEDs port for output
+    PORTD = 0xff;  // Turn on both LEDs
+
+    for (;;);
+
+    return 0;
+}
+
+ISR(INT4_vect)
+{
+    PORTD = (_BV(PORTD5)|_BV(PORTD7));
+}
+
+ISR(INT5_vect)
+{
+    PORTD = _BV(PORTD5);
+}
+
