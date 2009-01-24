12958e3473f783bfffa7ef88a6e27fffa9abdc73
kat.gunion <kat.gunion@1dc674fe-e43a-11dd-95e1-755cbddbdd66>
2009-01-24 21:49:40 +0000

    don't be a douche

---
+++ TestCode/Servo/servo.c
@@ -0,0 +1,29 @@
+/*
+ *  servo.c
+ *  
+ *
+ *  Created by Katherine Gunion on 24/01/09.
+ *  Copyright 2009 University of Victoria. All rights reserved.
+ *
+ */
+
+#include "servo.h"
+
+void
+servoInit(void){
+
+	TCCR1A = (0<<WGM10) | (1<<WGM11) | (1<<COM1B1) | (0<<COM1B0); 
+	/*Set the IO pins for output for  OCB1*/ 
+	DDRB = 0xFF;
+	/* Set the TOP value to 2500 this should mean that the TCNT1 counter
+	 should reset itself every 50 Hz at a clock of 1Mz for 8 Mhz, set to 20000(hopefully)*/
+	ICR1 = 20000;   
+	/* Set the low timings for the three output pins for ports A in OCR1A*/
+	OCR1B = 1600; //corresponds to 8% duty cycle or 2ms pulse width
+}
+
+
+void servoDuty(int duty){
+
+	OCR1B = duty;
+}
---
+++ TestCode/Servo/servo.h
@@ -0,0 +1,13 @@
+/*
+ *  servo.h
+ *  
+ *
+ *  Created by Katherine Gunion, Anthony Estey on 24/01/09.
+ *  Copyright 2009 University of Victoria. All rights reserved.
+ *
+ */
+
+
+
+void seroInit(void);
+void servoDuty(int duty);
