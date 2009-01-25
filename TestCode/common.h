035a12c65d3b7aad27f4c46500364f911a808740
kat.gunion <kat.gunion@1dc674fe-e43a-11dd-95e1-755cbddbdd66>
2009-01-25 04:49:38 +0000

    added dependancies for Jostick

---
+++ TestCode/common.h
@@ -0,0 +1,25 @@
+/**
+ * @file   common.h
+ *
+ * @brief Some common macros.
+ *
+ * CSC 460/560 Real Time Operating Systems - Mantis Cheng
+ *
+ * @author Scott Craig
+ * @author Justin Tanner
+ */
+
+
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+
+#define CLOCK_8MHZ()            CLKPR = (1<<CLKPCE); CLKPR = 0x00;
+
+//#define F_CPU                   8000000UL  // 8 MHz
+
+
+#define Disable_Interrupt()     asm volatile ("cli"::)
+#define Enable_Interrupt()     asm volatile ("sei"::)
+
+#endif
