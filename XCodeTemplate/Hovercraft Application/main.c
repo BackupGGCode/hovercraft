44730e92db7816bd235c847692750a70bdebf249
marcoy <marcoy@1dc674fe-e43a-11dd-95e1-755cbddbdd66>
2009-01-29 17:04:36 +0000

    Added XCode project template.

---
+++ XCodeTemplate/Hovercraft Application/main.c
@@ -0,0 +1,19 @@
+/**
+ * Main.
+ * 
+ * @author: ÇFULLUSERNAMEÈ
+ * @copyright: Copyright 2009 ÇFULLUSERNAMEÈ
+ * 
+ * License: <insert your license reference here>
+ */
+
+#include <avr/io.h>
+
+int main(void)
+{
+    /* insert your hardware initialization here */
+    for(;;){
+        /* insert your main loop code here */
+    }
+    return 0;   /* never reached */
+}
