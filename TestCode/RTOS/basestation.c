1a24a5722a0d73e092f44a3f9d00acdd7bb9355f
kat.gunion <kat.gunion@1dc674fe-e43a-11dd-95e1-755cbddbdd66>
2009-04-24 21:51:21 +0000

    all done

--- TestCode/RTOS/basestation.c
+++ TestCode/RTOS/basestation.c
@@ -14,16 +14,17 @@
 #define MOTOR_INFO_STRING "Right motor: %d, lett motor: %d\r\n"
 
 enum { A=1, B};
-const unsigned int PT = 2;
-const unsigned char PPP[] = {IDLE, 10, B, 100};
+const unsigned int PT = 0;
+const unsigned char PPP[] = {};
 volatile uint8_t radio_buffer[PAYLOAD_BYTES];
+infoPacket_t *info;
 
 int amount = 0;
 char toPrint[255]; 
 
-extern uint16_t trace_counter;
+
 EVENT* radio_event;
-EVENT* print_event;
+
 
 void 
 printInfo(infoPacket_t theInfo) 
@@ -35,39 +36,21 @@ printInfo(infoPacket_t theInfo)
     uart_write((uint8_t *)toPrint, amount);
 }
 
-void uart_task(void)
-{
-   
-    int arg = 0;
-   
-    arg = Task_GetArg();
+void 
 
-    for(;;)
+uart_task(void)
+{
+     for(;;)
     {
         Event_Wait(radio_event);
-        amount = sprintf(toPrint, "Inside the uart task\r\n");
-        uart_write((uint8_t*) toPrint, amount);
+      	printInfo(*info);
         Task_Next();
     }
 }
 
 
 
-void event_task(void) {
-    for(;;){
-        Event_Signal(radio_event);
-        Task_Next();
-        amount = sprintf(toPrint, "Inside the event trigger task\r\n");
-        uart_write((uint8_t*) toPrint, amount);
-        Task_Next();
-        amount = sprintf(toPrint, "Inside the event trigger task\r\n");
-        uart_write((uint8_t*) toPrint, amount);
-        Task_Next();
-        amount = sprintf(toPrint, "Inside the event trigger task\r\n");
-        uart_write((uint8_t*) toPrint, amount);
-        Task_Next();
-    }
-}
+
 
 
 
@@ -81,14 +64,11 @@ int main(void)
     sei();
 
     radio_event = Event_Init();
-    print_event = Event_Init();
+
 
     Task_Create(uart_task, 1, RR, A);
     Task_Create(event_task, 2, PERIODIC, B);
-//    Task_Create(generic_task, 3, RR, C);
-//    Task_Create(generic_task, 4, RR, D);
-//    Task_Create(generic_task, 5, RR, E);
-//    Task_Create(generic_task, 6, RR, F);
+
     
     return 0;
 }
@@ -106,11 +86,12 @@ ISR (INT4_vect)
     
     // Figure out what type of packet this is.
     genericPacket_t *incomingPacket = (genericPacket_t *)radio_buffer;
-    infoPacket_t *info;
+    
     switch(incomingPacket->type) {
         case INFO:
             info = (infoPacket_t *)radio_buffer;
-            printInfo(*info);
+            Event_Signal(radio_event);
+			
             break;
         default:
             // ignore
