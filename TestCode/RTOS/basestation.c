6457859663b4fe9359dcc7464d635545ffccbc1d
marcoy <marcoy@1dc674fe-e43a-11dd-95e1-755cbddbdd66>
2009-04-24 20:58:49 +0000

    Implemented functions for the basestation to receive and print packets.

--- TestCode/RTOS/basestation.c
+++ TestCode/RTOS/basestation.c
@@ -6,19 +6,34 @@
 
 #include "common.h"
 #include "OS/os.h"
+#include "radio/radio.h"
 #include "uart/uart.h"
 #include "trace/trace.h"
+
+#define SONAR_INFO_STRING "Front sonar: %d, right sonar: %d, left sonar: %d\r\n"
+#define MOTOR_INFO_STRING "Right motor: %d, lett motor: %d\r\n"
 
 enum { A=1, B};
 const unsigned int PT = 2;
 const unsigned char PPP[] = {IDLE, 10, B, 100};
+volatile uint8_t radio_buffer[PAYLOAD_BYTES];
 
 int amount = 0;
-char toPrint[250]; 
+char toPrint[255]; 
 
 extern uint16_t trace_counter;
 EVENT* radio_event;
 EVENT* print_event;
+
+void 
+printInfo(infoPacket_t theInfo) 
+{
+    amount = sprintf(toPrint, SONAR_INFO_STRING, theInfo.frontSonar, theInfo.rightSonar,
+                     theInfo.leftSonar);
+    uart_write((uint8_t *)toPrint, amount);
+    amount = sprintf(toPrint, MOTOR_INFO_STRING, theInfo.rightMotor, theInfo.leftMotor);
+    uart_write((uint8_t *)toPrint, amount);
+}
 
 void uart_task(void)
 {
@@ -38,11 +53,8 @@ void uart_task(void)
 
 
 
-void event_task(void){
-    
-    
+void event_task(void) {
     for(;;){
-        
         Event_Signal(radio_event);
         Task_Next();
         amount = sprintf(toPrint, "Inside the event trigger task\r\n");
@@ -61,10 +73,12 @@ void event_task(void){
 
 int main(void)
 {
+    cli();
     /* setup the test */
 	uart_init();
 	// uart_write((uint8_t*)"START\r\n", 9);
     // set_test(3);
+    sei();
 
     radio_event = Event_Init();
     print_event = Event_Init();
@@ -78,3 +92,35 @@ int main(void)
     
     return 0;
 }
+
+ISR (INT4_vect)
+{
+    int i;
+    
+    PORTD ^= _BV(PORTD5);
+    
+    for (i = 0; i < PAYLOAD_BYTES; i++)
+    {
+        radio_buffer[i] = radio_get_byte();
+    }
+    
+    // Figure out what type of packet this is.
+    genericPacket_t *incomingPacket = (genericPacket_t *)radio_buffer;
+    infoPacket_t *info;
+    switch(incomingPacket->type) {
+        case INFO:
+            info = (infoPacket_t *)radio_buffer;
+            printInfo(*info);
+            break;
+        default:
+            // ignore
+            break;
+    }
+    
+    /* print out the radio packet on uart */
+    //uart_write((uint8_t*)radio_buffer, PAYLOAD_BYTES);
+    
+    /* setup the radio to receive another packet */
+    radio_set_receive();
+}
+
